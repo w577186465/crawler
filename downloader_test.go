e8e7a5c8188fe5f983c924a767d71366e82c92c4
unknown <vsuper>
2017-12-11 18:15:28 +0800

    commit

--- downloader_test.go
+++ downloader_test.go
@@ -6,11 +6,12 @@ import (
 )
 
 func Test_download(t *testing.T) {
-	Download("http://www.zgchaye.cn/news/342/")
+	res := Download("http://www.zgchaye.cn/news/342/")
+	fmt.Println(res)
 }
 
 func Test_Html(t *testing.T) {
-	html, _ := Download("http://www.zgchaye.cn/news/342/").Html()
+	html, _ := Download("http://www.zgchaye.cn/baike/344p1/").Html()
 	title := html.Find("title").Text()
 	fmt.Println(title)
 }
